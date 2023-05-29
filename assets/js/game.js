5664a3c553836707c8400e6bab4a4a9f01ec0075
Leandro Leme dos Santos <leandrolemesantos@gmail.com>
2023-05-29 20:09:32 -0300

    147º dia de CTD - Avançando no algoritmo do game

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,4 @@
  const grid = document.querySelector('.grid');
-
  const characters = [
     'awake',
     'between',
@@ -14,32 +13,76 @@
 ];
 
  const createElement = (tag, className) => {
+  
  const element = document.createElement(tag);
  element.className = className;
  return element;
 }
 
-const createCard = (character) => {
+ let firstCard = '';
+ let secondCard = '';
+
+ const checkCards = () => {
+  const firstCharacter = firstCard.getAttribute('data-character');
+  const secondCharacter = secondCard.getAttribute('data-character');
+
+  if (firstCharacter=== secondCharacter) {
+
+
+  }else {
+    setTimeout(() => {
+
+      firstCard.classList.remove('reveal-card');
+      secondCard.classList.remove('reveal-card');
+
+    }, 600)
+
+  }
+
+ }
+
+ const revealCard = ({ target }) => {
+
+  if (target.parentNode.className.includes('reveal-card')) {
+    return;
+  }
+
+  if (firstCard === '') {
+    target.parentNode.classList.add('reveal-card');
+    firstCard = target.parentNode;
+
+  }else if (secondCard === '') {
+    target.parentNode.classList.add('reveal-card');
+    secondCard = target.parentNode;
+
+    checkCards();
+  }
+}
+
+ const createCard = (character) => {
  
- const card = createElement('div', 'card');
- const front= createElement('div', 'face front');
- const back = createElement('div', 'face back');
+  const card = createElement('div', 'card');
+  const front= createElement('div', 'face front');
+  const back = createElement('div', 'face back');
 
- front.style.backgroundImage = `url('/assets/img/${character}.jpg')`;
+  front.style.backgroundImage = `url('/assets/img/${character}.jpg')`;
 
- card.appendChild(front);
- card.appendChild(back);
+  card.appendChild(front);
+  card.appendChild(back);
 
- return card;
+  card.addEventListener('click', revealCard);
+  card.setAttribute('data-character', character)
+
+  return card;
 }
 
 const loadGame = () => {
+  const duplicateCharacters = [ ...characters, ...characters ];
+  const shuffleArray = duplicateCharacters.sort( () => Math.random() -0.5 );
  
-  characters.forEach((character) => {
-
-    const card = createCard(character);
-    grid.appendChild(card);
-
+  shuffleArray.forEach((character) => {
+  const card = createCard(character);
+  grid.appendChild(card);
  });     
 }
 
